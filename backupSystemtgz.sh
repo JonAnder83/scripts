2c3128a216443aaf0a54e65183ae9a5f8ed61c44
JonAnder83 <jonandermonleon@gmail.com>
2017-04-29 00:23:14 +0200

    Add files via upload

---
+++ backupSystemtgz.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# Small Script to backup the root system to a compressed tgz file- Change the destination directory to your needs.
+tar cvpzf /media/Storage/backupRex-$(date +%Y-%m-%d_%H%M%S).tgz --exclude=/proc --exclude=/lost+found --exclude=/mnt --exclude=/var/cache/pacman --exclude=/sys --exclude=/home --exclude=/.snapshots --exclude=/media /
